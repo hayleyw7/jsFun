cd5f1ac0824d0e8e723ca3da2501a5f9f4acecdf
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-08 16:24:26 -0600

    Fix indents

--- prototypes/index.js
+++ prototypes/index.js
@@ -443,7 +443,7 @@ const classPrompts = {
   //   { roomLetter: 'G', program: 'FE', capacity: 29 }
   // ]
 
-    feClassrooms() {
+  feClassrooms() {
 
     const result = [];
 
@@ -1113,13 +1113,13 @@ const bossPrompts = {
 
 
 
-    // Create an array of objects that each have the name of the boss and the sum
-    // loyalty of all their sidekicks. e.g.:
-    // [
-    //   { bossName: 'Jafar', sidekickLoyalty: 3 },
-    //   { bossName: 'Ursula', sidekickLoyalty: 20 },
-    //   { bossName: 'Scar', sidekickLoyalty: 16 }
-    // ]
+  // Create an array of objects that each have the name of the boss and the sum
+  // loyalty of all their sidekicks. e.g.:
+  // [
+  //   { bossName: 'Jafar', sidekickLoyalty: 3 },
+  //   { bossName: 'Ursula', sidekickLoyalty: 20 },
+  //   { bossName: 'Scar', sidekickLoyalty: 16 }
+  // ]
 
   bossLoyalty() {
 
@@ -1219,18 +1219,18 @@ const astronomyPrompts = {
 
 
 
-    // Return an array of the names of the constellations that the brightest stars are part of e.g.
+  // Return an array of the names of the constellations that the brightest stars are part of e.g.
 
-    //  [ "Canis Major",
-    //    "Carina",
-    //    "Boötes",
-    //    "Auriga",
-    //    "Orion",
-    //    "Lyra",
-    //    "Canis Minor",
-    //    "The Plow",
-    //    "Orion",
-    //    "The Little Dipper" ]
+  //  [ "Canis Major",
+  //    "Carina",
+  //    "Boötes",
+  //    "Auriga",
+  //    "Orion",
+  //    "Lyra",
+  //    "Canis Minor",
+  //    "The Plow",
+  //    "Orion",
+  //    "The Little Dipper" ]
 
   constellationsStarsExistIn() {
 
@@ -1363,30 +1363,30 @@ const dinosaurPrompts = {
 
 
 
-    /* Return an object where each key is a movie director's name and each value is
-        an object whose key is a movie's title and whose value is the average age
-        of the cast on the release year of that movie.
-      e.g.:
-      {
-        'Steven Spielberg':
-          {
-            'Jurassic Park': 34,
-            'The Lost World: Jurassic Park': 37
+  /* Return an object where each key is a movie director's name and each value is
+      an object whose key is a movie's title and whose value is the average age
+      of the cast on the release year of that movie.
+    e.g.:
+    {
+      'Steven Spielberg':
+        {
+          'Jurassic Park': 34,
+          'The Lost World: Jurassic Park': 37
+        },
+      'Joe Johnston':
+        {
+          'Jurassic Park III': 44
+        },
+      'Colin Trevorrow':
+        {
+          'Jurassic World': 56
           },
-        'Joe Johnston':
-          {
-            'Jurassic Park III': 44
-          },
-        'Colin Trevorrow':
-          {
-            'Jurassic World': 56
-           },
-        'J. A. Bayona':
-          {
-            'Jurassic World: Fallen Kingdom': 59
-          }
-      }
-    */
+      'J. A. Bayona':
+        {
+          'Jurassic World: Fallen Kingdom': 59
+        }
+    }
+  */
 
   averageAgePerMovie() {
 
@@ -1455,20 +1455,20 @@ const dinosaurPrompts = {
 
 
 
-    /*
-    Return an array of objects for each human and the age(s) they were in the movie(s) they were cast in, as an array of age(s). Only include humans who were cast in at least one movie.
+  /*
+  Return an array of objects for each human and the age(s) they were in the movie(s) they were cast in, as an array of age(s). Only include humans who were cast in at least one movie.
 
-    e.g.
-    [ { name: 'Sam Neill', ages: [ 46, 54 ] },
-      { name: 'Laura Dern', ages: [ 26, 34 ] },
-      { name: 'Jeff Goldblum', ages: [ 41, 45, 63, 66 ] },
-      { name: 'Richard Attenborough', ages: [ 70, 74, 92, 95 ] },
-      { name: 'Ariana Richards', ages: [ 14, 18 ] },
-      { name: 'Joseph Mazello', ages: [ 10, 14 ] },
-      { name: 'BD Wong', ages: [ 33, 55, 58 ] },
-      { name: 'Chris Pratt', ages: [ 36, 39 ] },
-      { name: 'Bryce Dallas Howard', ages: [ 34, 37 ] } ]
-    */
+  e.g.
+  [ { name: 'Sam Neill', ages: [ 46, 54 ] },
+    { name: 'Laura Dern', ages: [ 26, 34 ] },
+    { name: 'Jeff Goldblum', ages: [ 41, 45, 63, 66 ] },
+    { name: 'Richard Attenborough', ages: [ 70, 74, 92, 95 ] },
+    { name: 'Ariana Richards', ages: [ 14, 18 ] },
+    { name: 'Joseph Mazello', ages: [ 10, 14 ] },
+    { name: 'BD Wong', ages: [ 33, 55, 58 ] },
+    { name: 'Chris Pratt', ages: [ 36, 39 ] },
+    { name: 'Bryce Dallas Howard', ages: [ 34, 37 ] } ]
+  */
 
   actorsAgesInMovies() {
 
